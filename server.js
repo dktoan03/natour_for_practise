0b3bd3ce986570433c9b1fc0157ebc82a9204f21
Toan Duong <dktoan03@gmail.com>
2024-04-18 10:52:58 +0700

    updated file

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose
   })
   .then(() => console.log('DB connection established'))
 
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
